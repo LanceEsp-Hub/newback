e0f9145333bc21453c6aad4477cbc70d9e25187b
Lance Cedrick F. Espina <lancecedrickespina@gmail.com>
2025-07-16 04:11:55 +0800

    Delete Python/ass1/task1.py

--- Python/ass1/task1.py
+++
@@ -1,7 +0,0 @@
-# task1.py
-students = {'John', 'Maria', 'David', 'Samantha'}
-index = 2  # assuming index 2 is inputted
-
-# Convert set to list to access by index
-students_list = list(students)
-print("Student at index", index, "is:", students_list[index])
