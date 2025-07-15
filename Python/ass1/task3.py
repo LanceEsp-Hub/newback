2c073b80fa08931a9439645d777a51bc6153ce79
Lance Cedrick F. Espina <lancecedrickespina@gmail.com>
2025-07-16 04:12:42 +0800

    Delete Python/ass1/task3.py

--- Python/ass1/task3.py
+++
@@ -1,8 +0,0 @@
-# task3.py
-people = {'profile': {'John': 19, 'Emily': 21, 'Sarah': 25, 'Tom': 18}}
-
-# Print names of people older than 19
-print("People older than 19:")
-for name, age in people['profile'].items():
-    if age > 19:
-        print(name)
